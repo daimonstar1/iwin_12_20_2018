2df0703bca84ac373eb034bf0492442bb35d7a09
HOS-IT-PC15660\SuUs <thanhpt19@vpbank.com.vn>
2018-12-21 12:07:22 +0700

    Initial commit

---
+++ Assets/GameTacoSDK/Scripts/Manage/Loading/TacoManageLoadingView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameTacoSDK
+{
+	public interface ITacoLoadingState
+	{
+		void showLoading (string message);
+
+		void hideLoading (string message);
+	}
+
+	public class TacoManageLoadingView : MonoBehaviour,ITacoLoadingState
+	{
+		#region ITacoLoadingState implementation
+
+		public void showLoading (string message)
+		{
+			Debug.Log (message);
+		}
+
+		public void hideLoading (string message)
+		{
+			Debug.Log (message);
+		}
+
+		#endregion
+
+
+	}
+}
---
+++ Assets/GameTacoSDK/Scripts/Manage/TacoSDK.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameTacoSDK
+{
+	public class TacoSDK : SingletonMono<TacoSDK>
+	{
+
+		public void Init ()
+		{
+			Instantiate (Resources.Load<GameObject> ("Prefabs/LoginPanel"));
+		}
+	}
+}
